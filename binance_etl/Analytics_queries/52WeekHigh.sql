69d6dae7a3ddb5c74aa646e4b4db860242836ec1
amogh147 <amogh147@users.noreply.github.com>
2021-10-25 15:39:32 -0400

    Update 52WeekHigh.sql

--- binance_etl/Analytics_queries/52WeekHigh.sql
+++ binance_etl/Analytics_queries/52WeekHigh.sql
@@ -1,7 +1,7 @@
 --Question: What is the 52 week high and low for all the items sold in the past 3 months?
-with orders_sold as 
-(
-	select
-	from fct_Accountorders fao join dim_status ds
-	on fao.status_key = ds.status_key and lower(ds.status_key) = 'sold'
-)
+
+select max(52week_high) as 52week_high, min(52week_low) as 52week_low
+from fct_candles1m fc join dim_status ds
+(on fc.status_key = ds.status_key and lower(ds.status_key) = 'sold')
+join dim_date on (dd.date_key = fc.date_key)
+where datediff(months, dd.date_value, current_date) <= 3
